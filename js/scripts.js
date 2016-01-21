905f0aedd2e455eb1740e15df6a241031a690c5e
T <T@epicodus.com>
2016-01-21 14:09:08 -0800

    add change array function

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,91 @@
 // var master = function(num) {
 
-var oneDigit = function(num) {
+
+var changeArr = function(num) {
   var str = num.toString();
   var arr = str.split("");
   for (var i = 0; i < arr.length; i++) {
      arr[i] = parseInt(arr[i]);
- }
+  };
+  return arr;
+};
+
+
+var oneDigit = function(num) {
+
+  var arr = changeArr(num);
+
   console.log(arr);
   if (arr.length === 1) {
      if (arr[0] === 1) {
-       var final = "I"
+       var final = "I";
      } else if (arr[0] === 2) {
-       final = "II"
+       final = "II";
      } else if (arr[0] === 3) {
-       final = "III"
+       final = "III";
      } else if (arr[0] === 4) {
-       final = "IV"
+       final = "IV";
      } else if (arr[0] === 5) {
-       final = "V"
+       final = "V";
      } else if (arr[0] === 6) {
-       final = "VI"
+       final = "VI";
      } else if (arr[0] === 7) {
-       final = "VII"
+       final = "VII";
      } else if (arr[0] === 8) {
-       final = "VIII"
+       final = "VIII";
      } else if (arr[0] === 9) {
-       final = "IX"
+       final = "IX";
      }
    }
- return final
+ return final;
 
 }
 
+
 var twoDigit = function(num) {
-  var str = num.toString();
-  var arr = str.split("");
-  for (var i = 0; i < arr.length; i++) {
-     arr[i] = parseInt(arr[i]);
- }
+  var arr = changeArr(num);
+
   console.log(arr);
   if (arr.length === 2) {
      if (arr[0] === 1) {
-       var final = "X"
+       var final = "X";
      } else if (arr[0] === 2) {
-       final = "XX"
+       final = "XX";
      } else if (arr[0] === 3) {
-       final = "XXX"
+       final = "XXX";
      } else if (arr[0] === 4) {
-       final = "XL"
+       final = "XL";
      } else if (arr[0] === 5) {
-       final = "L"
+       final = "L";
      } else if (arr[0] === 6) {
-       final = "LX"
+       final = "LX";
      } else if (arr[0] === 7) {
-       final = "LXX"
+       final = "LXX";
      } else if (arr[0] === 8) {
-       final = "LXXX"
+       final = "LXXX";
      } else if (arr[0] === 9) {
-       final = "XC"
+       final = "XC";
      }
 
 
      if (arr[1] === 1) {
-       var final1 = "I"
+       var final1 = "I";
      } else if (arr[1] === 2) {
-       final1 = "II"
+       final1 = "II";
      } else if (arr[1] === 3) {
-       final1 = "III"
+       final1 = "III";
      } else if (arr[1] === 4) {
-       final1 = "IV"
+       final1 = "IV";
      } else if (arr[1] === 5) {
-       final1 = "V"
+       final1 = "V";
      } else if (arr[1] === 6) {
-       final1 = "VI"
+       final1 = "VI";
      } else if (arr[1] === 7) {
-       final1 = "VII"
+       final1 = "VII";
      } else if (arr[1] === 8) {
-       final1 = "VIII"
+       final1 = "VIII";
      } else if (arr[1] === 9) {
-       final1 = "IX"
+       final1 = "IX";
      }
 
 
@@ -87,181 +94,173 @@ var twoDigit = function(num) {
 
 
    }
-   return(final + final1)
+   return(final + final1);
 
 }
 
 var threeDigit = function(num) {
-  var str = num.toString();
-  var arr = str.split("");
-  for (var i = 0; i < arr.length; i++) {
-     arr[i] = parseInt(arr[i]);
- }
+  var arr = changeArr(num);
+
   console.log(arr);
   if (arr.length === 3) {
      if (arr[0] === 1) {
-       var final = "C"
+       var final = "C";
      } else if (arr[0] === 2) {
-       final = "CC"
+       final = "CC";
      } else if (arr[0] === 3) {
-       final = "CCC"
+       final = "CCC";
      } else if (arr[0] === 4) {
-       final = "CD"
+       final = "CD";
      } else if (arr[0] === 5) {
-       final = "D"
+       final = "D";
      } else if (arr[0] === 6) {
-       final = "DC"
+       final = "DC";
      } else if (arr[0] === 7) {
-       final = "DCC"
+       final = "DCC";
      } else if (arr[0] === 8) {
-       final = "DCCC"
+       final = "DCCC";
      } else if (arr[0] === 9) {
-       final = "CM"
+       final = "CM";
      }
 
 
      if (arr[1] === 1) {
-       var final1 = "X"
+       var final1 = "X";
      } else if (arr[1] === 2) {
-       final1 = "XX"
+       final1 = "XX";
      } else if (arr[1] === 3) {
-       final1 = "XXX"
+       final1 = "XXX";
      } else if (arr[1] === 4) {
-       final1 = "XL"
+       final1 = "XL";
      } else if (arr[1] === 5) {
-       final1 = "L"
+       final1 = "L";
      } else if (arr[1] === 6) {
-       final1 = "LX"
+       final1 = "LX";
      } else if (arr[1] === 7) {
-       final1 = "LXX"
+       final1 = "LXX";
      } else if (arr[1] === 8) {
-       final1 = "LXXX"
+       final1 = "LXXX";
      } else if (arr[1] === 9) {
-       final1 = "XC"
+       final1 = "XC";
      }
 
      if (arr[2] === 1) {
-       var final2 = "I"
+       var final2 = "I";
      } else if (arr[2] === 2) {
-       final2 = "II"
+       final2 = "II";
      } else if (arr[2] === 3) {
-       final2 = "III"
+       final2 = "III";
      } else if (arr[2] === 4) {
-       final2 = "IV"
+       final2 = "IV";
      } else if (arr[2] === 5) {
-       final2 = "V"
+       final2 = "V";
      } else if (arr[2] === 6) {
-       final2 = "VI"
+       final2 = "VI";
      } else if (arr[2] === 7) {
-       final2 = "VII"
+       final2 = "VII";
      } else if (arr[2] === 8) {
-       final2 = "VIII"
+       final2 = "VIII";
      } else if (arr[2] === 9) {
-       final2 = "IX"
+       final2 = "IX";
      }
-
-
-
-
-
 
    }
    return(final + final1 + final2)
 
 }
 var fourDigit = function(num) {
-  var str = num.toString();
-  var arr = str.split("");
-  for (var i = 0; i < arr.length; i++) {
-     arr[i] = parseInt(arr[i]);
- }
+  var arr = changeArr(num);
+
   console.log(arr);
   if (arr.length === 4) {
      if (arr[0] === 1) {
-       var final = "M"
+       var final = "M";
      } else if (arr[0] === 2) {
-       final = "MM"
+       final = "MM";
      } else if (arr[0] === 3) {
-       final = "MMM"
+       final = "MMM";
      } else if (arr[0] === 4) {
-       final = "CD"
+       final = "CD";
      } else if (arr[0] === 5) {
-       final = "D"
+       final = "D";
      } else if (arr[0] === 6) {
-       final = "DC"
+       final = "DC";
      } else if (arr[0] === 7) {
-       final = "DCC"
+       final = "DCC";
      } else if (arr[0] === 8) {
-       final = "DCCC"
+       final = "DCCC";
      } else if (arr[0] === 9) {
-       final = "CM"
+       final = "CM";
      }
 
      if (arr[1] === 1) {
-       var final1 = "C"
+       var final1 = "C";
      } else if (arr[1] === 2) {
-       final1 = "CC"
+       final1 = "CC";
      } else if (arr[1] === 3) {
-       final1 = "CCC"
+       final1 = "CCC";
      } else if (arr[1] === 4) {
-       final1 = "CD"
+       final1 = "CD";
      } else if (arr[1] === 5) {
-       final1 = "D"
+       final1 = "D";
      } else if (arr[1] === 6) {
-       final1 = "DC"
+       final1 = "DC";
      } else if (arr[1] === 7) {
-       final1 = "DCC"
+       final1 = "DCC";
      } else if (arr[1] === 8) {
-       final1 = "DCCC"
+       final1 = "DCCC";
      } else if (arr[1] === 9) {
-       final1 = "CM"
+       final1 = "CM";
      }
 
      if (arr[2] === 1) {
-       var final2 = "X"
+       var final2 = "X";
      } else if (arr[2] === 2) {
-       final2 = "XX"
+       final2 = "XX";
      } else if (arr[2] === 3) {
-       final2 = "XXX"
+       final2 = "XXX";
      } else if (arr[2] === 4) {
-       final2 = "XL"
+       final2 = "XL";
      } else if (arr[2] === 5) {
-       final2 = "L"
+       final2 = "L";
      } else if (arr[2] === 6) {
-       final2= "LX"
+       final2= "LX";
      } else if (arr[2] === 7) {
-       final2 = "LXX"
+       final2 = "LXX";
      } else if (arr[2] === 8) {
-       final2 = "LXXX"
+       final2 = "LXXX";
      } else if (arr[2] === 9) {
-       final2 = "XC"
+       final2 = "XC";
      }
 
      if (arr[3] === 1) {
-       var final3 = "I"
+       var final3 = "I";
      } else if (arr[3] === 2) {
-       final3 = "II"
+       final3 = "II";
      } else if (arr[3] === 3) {
-       final3 = "III"
+       final3 = "III";
      } else if (arr[3] === 4) {
-       final3 = "IV"
+       final3 = "IV";
      } else if (arr[3] === 5) {
-       final3 = "V"
+       final3 = "V";
      } else if (arr[3] === 6) {
-       final3 = "VI"
+       final3 = "VI";
      } else if (arr[3] === 7) {
-       final3 = "VII"
+       final3 = "VII";
      } else if (arr[3] === 8) {
-       final3 = "VIII"
+       final3 = "VIII";
      } else if (arr[3] === 9) {
-       final3 = "IX"
+       final3 = "IX";
      }
 
-
-
-
-
    }
-   return(final + final1 + final2 + final3)
+   return(final + final1 + final2 + final3);
 
 }
+
+$(document).ready(function() {
+  $("form#numeralProcess").submit(function(event) {
+
+    (event).preventDefault();
+  })
+});
